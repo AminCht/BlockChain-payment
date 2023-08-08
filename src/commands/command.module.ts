1008d86e0f7fe0fe1166a0eca5ced45ae251d836
AminCht <amincht81@gmail.com>
2023-08-09 01:52:50 +0330

    add check balance command

--- src/commands/command.module.ts
+++ src/commands/command.module.ts
@@ -3,9 +3,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Wallet } from '../database/entities/Wallet.entity';
 import databaseModule from '../database/database.module';
+import { CheckBallanceCommand } from './checkBalanceCommand';
+import { Transaction } from '../database/entities/Transaction.entity';
 
 @Module({
-  imports: [databaseModule, TypeOrmModule.forFeature([Wallet])],
-  providers: [CreateWalletCommand],
+  imports: [databaseModule, TypeOrmModule.forFeature([Wallet,Transaction])],
+  providers: [CreateWalletCommand,CheckBallanceCommand],
 })
 export class CommandModule {}
