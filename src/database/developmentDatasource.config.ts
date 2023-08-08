d9b1eb50a97bd8798d1c6eff9f0e4a868251bdf3
foadas <foad1381@gmail.com>
2023-08-09 00:06:08 +0430

    added docker compose

--- src/database/developmentDatasource.config.ts
+++ src/database/developmentDatasource.config.ts
@@ -5,11 +5,16 @@ config();
 const configService = new ConfigService();
 
 export const developmentDbConfig = {
-  type: 'sqlite',
-  database: `${process.cwd()}/db.sqlite`,
+  type: 'postgres',
+  host: process.env.STAGE_DB_HOST,
+  port: process.env.STAGE_DB_PORT,
+  username: process.env.STAGE_DB_USERNAME,
+  password: process.env.STAGE_DB_PASSWORD,
+  database: process.env.STAGE_DB_NAME,
+  synchronize: true,
+  logging: true,
   entities: [__dirname + '/entities/*.entity{.js,.ts}'],
   migrations: [__dirname + '/../../dist/database/dev_migrations/*.js'],
-  logging: false,
   migrationsTableName: 'migration',
   cli: {
     migrationsDir: __dirname + '/dev_migrations/',
