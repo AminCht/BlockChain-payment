1da1ff5bd3d47dd8d8780dc46f87ffd3ef528e3f
AminCht <amincht81@gmail.com>
2023-08-09 13:44:41 +0330

    add test db

--- src/database/testDatasource.config.ts
+++ src/database/testDatasource.config.ts
@@ -5,11 +5,20 @@ config();
 const configService = new ConfigService();
 
 export const testDbConfig = {
-  type: 'better-sqlite3',
-  database: ':memory:',
-  dropSchema: true,
-  synchronize: true,
-  entities: [__dirname + '/entities/*.entity.{js,ts}'],
+  type: 'postgres',
+  host: process.env.TEST_DB_HOST,
+  port: process.env.TEST_DB_PORT,
+  username: process.env.TEST_DB_USERNAME,
+  password: process.env.TEST_DB_PASSWORD,
+  database: process.env.TEST_DB_NAME,
+  synchronize: false,
+  logging: true,
+  entities: [__dirname + '/entities/*.entity{.js,.ts}'],
+  migrations: [__dirname + '/../../dist/database/test_migrations/*.js'],
+  migrationsTableName: 'migration',
+  cli: {
+    migrationsDir: __dirname + '/dev_migrations/',
+  },
 };
 
 export default new DataSource(testDbConfig as DataSourceOptions);
