8b07ff0c495ffaf3bfbb3d01b10806bbe2e54d7f
foadas <foad1381@gmail.com>
2023-08-09 13:58:03 -0400

    test is not working

---
+++ src/database/test_migrations/1691598849369-test_migrations.ts
@@ -0,0 +1,18 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class TestMigrations1691598849369 implements MigrationInterface {
+    name = 'TestMigrations1691598849369'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "Transctions" ("id" SERIAL NOT NULL, "amount" integer NOT NULL, "network" character varying NOT NULL, "currency" character varying NOT NULL, "wallet_balance_before" numeric NOT NULL, "wallet_balance_after" numeric, "createdDate" TIMESTAMP NOT NULL DEFAULT now(), "expireTime" TIMESTAMP NOT NULL, "walletId" integer, CONSTRAINT "REL_531ef47a3dd72d14cb1ef2a93b" UNIQUE ("walletId"), CONSTRAINT "PK_d1d68b8237d2f3d4d8d68b78959" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "Wallets" ("id" SERIAL NOT NULL, "address" character varying NOT NULL, "private_key" character varying NOT NULL, "wallet_network" character varying NOT NULL, "type" character varying NOT NULL, "lock" boolean NOT NULL DEFAULT false, "status" boolean NOT NULL DEFAULT '1', CONSTRAINT "UQ_b4f4eb0f6ea306e6bd8b3b71b9a" UNIQUE ("address"), CONSTRAINT "UQ_e61467f4042c97270174fe080e2" UNIQUE ("private_key"), CONSTRAINT "PK_22643866c3dcd5442c341d43b67" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`ALTER TABLE "Transctions" ADD CONSTRAINT "FK_531ef47a3dd72d14cb1ef2a93ba" FOREIGN KEY ("walletId") REFERENCES "Wallets"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "Transctions" DROP CONSTRAINT "FK_531ef47a3dd72d14cb1ef2a93ba"`);
+        await queryRunner.query(`DROP TABLE "Wallets"`);
+        await queryRunner.query(`DROP TABLE "Transctions"`);
+    }
+
+}
--- src/payment/payment.service.spec.ts
+++ src/payment/payment.service.spec.ts
@@ -1,20 +1,31 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PaymentService } from './payment.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { getRepositoryToken, TypeOrmModule } from "@nestjs/typeorm";
+import { Wallet } from "../database/entities/Wallet.entity";
+import { Transaction } from "../database/entities/Transaction.entity";
+import { Repository } from "typeorm";
+import DatabaseModule from "../database/database.module";
+import * as process from "process";
 describe('PaymentService', () => {
   let service: PaymentService;
+  let walletRepo: Repository<Wallet>;
+  //let transactionRepo: Repository<Transaction>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        DatabaseModule,
+        TypeOrmModule.forFeature([Wallet, Transaction]),
+      ],
       providers: [PaymentService],
-      imports: [],
     }).compile();
-
     service = module.get<PaymentService>(PaymentService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+    //expect(walletRepo).toBeDefined();
+   // expect(transactionRepo).toBeDefined();
   });
   describe('Test create payment', () => {
     it('should create a payment and return wallet address and id', async () => {
