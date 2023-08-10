51db9c83805f6649a6ef58c09d0d078e0c8d20e9
foadas <foad1381@gmail.com>
2023-08-10 06:24:09 -0400

    check balance

--- src/commands/checkBalanceCommand.ts
+++ src/commands/checkBalanceCommand.ts
@@ -3,12 +3,15 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Wallet as WalletEntity } from '../database/entities/Wallet.entity';
 import { Repository } from 'typeorm';
 import { Transaction } from '../database/entities/Transaction.entity';
+import { InfuraProvider } from "ethers/lib.esm";
 
 @Command({ name: 'check-balance' })
 export class CheckBallanceCommand extends CommandRunner {
   constructor(
-    @InjectRepository(Transaction) private readonly transactionRepo: Repository<Transaction>,
-    @InjectRepository(WalletEntity) private readonly walletRepo: Repository<WalletEntity> 
+    @InjectRepository(Transaction)
+    private readonly transactionRepo: Repository<Transaction>,
+    @InjectRepository(WalletEntity)
+    private readonly walletRepo: Repository<WalletEntity>,
   ) {
     super();
   }
@@ -16,30 +19,54 @@ export class CheckBallanceCommand extends CommandRunner {
     passedParams: string[],
     options?: Record<string, any>,
   ): Promise<void> {
+    const provider = this.infuraConnect();
     const wallet = await this.walletRepo.find({
-        where:{
-            lock:true
-        }
+      where: {
+        lock: true,
+      },
     });
-    for(var walletItem of wallet){
-        await this.checkBalance(walletItem);
+    for (const walletItem of wallet) {
+      await this.checkBalance(walletItem, provider);
     }
   }
 
-  async checkBalance(walletItem: WalletEntity){
+  async checkBalance(walletItem: WalletEntity, provider: InfuraProvider) {
     const transaction = await this.transactionRepo.findOne({
-        where:{
-            wallet:{
-                id:walletItem.id
-            }
-        }
+      where: {
+        wallet: {
+          id: walletItem.id,
+        },
+      },
     });
-    const amount = BigInt(transaction.wallet_balance_after) - BigInt(transaction.wallet_balance_before );
-    if(BigInt(transaction.amount)==amount){
-        walletItem.lock = false;
-        transaction.status = 'Successfully';
-        await this.transactionRepo.save(transaction);
-        await this.walletRepo.save(walletItem);
+    const currentBalance = await this.getCurrentBalance(
+      transaction.wallet.address,
+      provider,
+    );
+    const expectedAmount =
+      BigInt(currentBalance) - BigInt(transaction.wallet_balance_before);
+    if (expectedAmount == BigInt(transaction.amount)) {
+      walletItem.lock = false;
+      transaction.status = 'Successfully';
+      await this.transactionRepo.save(transaction);
+      await this.walletRepo.save(walletItem);
+    } else if (
+      expectedAmount > BigInt(transaction.amount) ||
+      expectedAmount < BigInt(transaction.amount)
+    ) {
+      walletItem.lock = false;
+      transaction.status = 'failed';
+      await this.transactionRepo.save(transaction);
+      await this.walletRepo.save(walletItem);
     }
+  }
+  infuraConnect() {
+    return new InfuraProvider(process.env.DEV_NETWORK, process.env.DEV_API_KEY);
+  }
+  async getCurrentBalance(
+    address: string,
+    provider: InfuraProvider,
+  ): Promise<string> {
+    const balancePromise = await provider.getBalance(address);
+    return balancePromise.toString();
   }
 }
