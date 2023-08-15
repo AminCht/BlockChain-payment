14e5184c3361511da0baaebe1bf6dceefa63357b
AminCht <amincht81@gmail.com>
2023-08-15 14:30:53 +0330

    add signup

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { PaymentModule } from './payment/payment.module';
 import { WalletModule } from './wallet/wallet.module';
 import { TransactionModule } from './transaction/transaction.module';
+import { AuthModule } from './auth/auth.module';
 import DatabaseModule from './database/database.module';
 
 @Module({
-    imports: [DatabaseModule, PaymentModule, WalletModule, TransactionModule],
+    imports: [DatabaseModule, PaymentModule, WalletModule, TransactionModule, AuthModule],
     controllers: [AppController],
     providers: [AppService],
 })
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,51 @@
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { User } from '../database/entities/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { AuthDto } from './dto/auth.dto';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class AuthService {
+    constructor(
+        @InjectRepository(User) private userRepo: Repository<User>,
+        private jwt: JwtService
+        ){}
+
+    async signUp(dto: AuthDto){
+        try{
+            const password = await this.hashPassword(dto.password)
+            const user = this.userRepo.create({
+                username: dto.username,
+                password:password
+            });
+            await this.userRepo.save(user);
+            return this.signToken(user.id,user.username);
+        } catch(error){
+            if(error.code === 'P2002'){
+                throw new ForbiddenException('This UserName has already taken');
+            }
+        }
+    }
+
+    async login(dto: AuthDto){
+        
+    }
+
+    async signToken(id: number, username: string){
+        const payload = { username:username, id:id, exp: Math.floor(Date.now() / 1000) + 60 * 60 * 24 * 20 };
+        const token = await this.jwt.signAsync(payload);
+        return {accesstoken: token};
+    }
+
+    async hashPassword(password: string){
+        const hashRounds = 20
+        for (let i = 0; i < hashRounds; i++) {
+            password = await bcrypt.hash(password, 10);
+        }
+        return password;
+    }
+
+
+}
