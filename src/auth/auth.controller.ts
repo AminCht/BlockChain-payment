aa61f3ea2952c91ff952b998de2c301da19f927b
AminCht <amincht81@gmail.com>
2023-08-16 01:04:49 +0330

    set cookie for auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,19 +1,20 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Response } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto/auth.dto';
+import { Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
     constructor(private authService: AuthService){}
 
     @Post('signup')
-    async signUp(@Body() dto:AuthDto){
-        return await this.authService.signUp(dto);
+    async signUp(@Body() dto:AuthDto, @Response() res: Response){
+        return await this.authService.signUp(dto, res);
     }
 
     @Post('login')
     async login(@Body() dto: AuthDto){
-        return await this.authService.login(dto);
+        return await this.authService.login(dto , res);
     }
 
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -5,6 +5,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { AuthDto } from './dto/auth.dto';
 import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
+import { Response } from 'express';
 
 @Injectable()
 export class AuthService {
@@ -13,7 +14,7 @@ export class AuthService {
         private jwt: JwtService,
     ) {}
 
-    public async signUp(dto: AuthDto) {
+    public async signUp(dto: AuthDto, res: Response) {
         try {
             const hashPassword = await this.hashPassword(dto.password);
             const user = this.userRepo.create({
@@ -21,7 +22,7 @@ export class AuthService {
                 password: hashPassword,
             });
             await this.userRepo.save(user);
-            return this.signToken(user.id, user.username);
+            return this.signToken(user.id, user.username, res);
         } catch (error) {
             if (error.code === 'P2002') {
                 throw new ForbiddenException('This UserName has already taken');
@@ -49,9 +50,10 @@ export class AuthService {
         throw new ForbiddenException('username or password is incorrect');
         
     }
-    private async signToken(id: number, username: string) {
+    private async signToken(id: number, username: string, res: Response) {
         const payload = { username:username, id:id, exp: Math.floor(Date.now() / 1000) + 60 * 60 * 24 * 20 };
         const token = await this.jwt.signAsync(payload);
+        this.setCookie(res, token);
         return { access_token: token };
     }
 
@@ -62,5 +64,11 @@ export class AuthService {
         const saltRounds = 10
         
         return await bcrypt.hash(password,saltRounds);
+    }
+
+    setCookie(res: Response, token: string){
+        res.cookie('accessToken', token, {
+            httpOnly: true,
+        });
     }
 }
