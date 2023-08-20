c46b9a8b9f90225d68adad00c2a6eed0202a424e
AminCht <amincht81@gmail.com>
2023-08-20 14:47:24 +0330

    add id to get all tokens

--- src/access/access.service.ts
+++ src/access/access.service.ts
@@ -15,7 +15,7 @@ export class AccessService {
     ) {}
     public async getAllSupportedTokens(): Promise<Currency[]> {
         const tokens = await this.currencyRepo.find({
-            select: ['network', 'symbol', 'name', 'status'],
+            select: ['id', 'network', 'symbol', 'name', 'status'],
             where: {status: true}
         });
         return tokens;
