4e6e153cb28017bcb18652f62c50596a8ab46e0f
AminCht <amincht81@gmail.com>
2023-08-20 08:53:41 +0330

    add admin signup logic

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -5,11 +5,12 @@ import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../database/entities/User.entity';
 import { JwtStrategy } from '../auth/strategy/jwt.strategy';
+import { AuthService } from '../auth/auth.service';
 
 @Module({
   imports:[JwtModule.register({secret: process.env.JWT_SECRET_ADMIN,signOptions: { expiresIn: '20d' }}),
   TypeOrmModule.forFeature([User])],
   controllers: [AdminController],
-  providers: [AdminService, JwtStrategy]
+  providers: [AdminService, JwtStrategy, AuthService]
 })
 export class AdminModule {}
--- src/admin/admin.service.ts
+++ src/admin/admin.service.ts
@@ -1,10 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from '../database/entities/User.entity';
+import { Repository } from 'typeorm';
+import { JwtService } from '@nestjs/jwt';
+import { CreateAdminRequestDto } from './dto/createAdmin.dto';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable()
 export class AdminService {
     constructor(
-        
+        @InjectRepository(User) private userRepo: Repository<User>,
+        private jwt: JwtService,
+        private authService: AuthService
     ){}
-
+    
+    async createAdmin(dto: CreateAdminRequestDto){
+        try{
+            const hashedPassword = await this.authService.hashPassword(dto.password);
+            const admin = this.userRepo.create({
+                username: dto.username,
+                password: hashedPassword
+            });
+            await this.userRepo.save(admin);
+        } catch(error){
+            if (error.code === '23505') {
+                throw new ForbiddenException('This UserName has already taken');
+            }
+            throw error;
+        }
+    }
 
 }
