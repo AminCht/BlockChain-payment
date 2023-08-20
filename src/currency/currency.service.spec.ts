34c73437d3f07ab8c65262e8210e7ebca8a14aeb
foadas <foad1381@gmail.com>
2023-08-21 04:12:25 +0430

    currency service

--- src/currency/currency.service.spec.ts
+++ src/currency/currency.service.spec.ts
@@ -15,4 +15,28 @@ describe('CurrencyService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+  describe('getCurrencies',()=>{
+    it('should return all currencies',()=>{
+
+    })
+  })
+  describe('getCurrencies by id',()=>{
+    it('should return a currency',()=>{
+
+    })
+  })
+  describe('add currency',()=>{
+    it('should add a currency',()=>{
+
+    })
+  })
+  describe('update currency',()=>{
+    it('should update a currency',()=>{
+
+    })
+  })
+  describe('delete currency',()=>{
+    it('should delete a currency',()=>{
+
+    });
 });
--- src/currency/currency.service.ts
+++ src/currency/currency.service.ts
@@ -1,4 +1,41 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import {Repository, UpdateResult} from 'typeorm';
+import { Currency } from '../database/entities/Currency.entity';
+import { CurrencyDto, UpdateCurrencyDto} from './dto/Currency.dto';
 
 @Injectable()
-export class CurrencyService {}
+export class CurrencyService {
+    constructor(@InjectRepository(Currency) private currencyRepo: Repository<Currency>) {}
+    public async getAllCurrencies(): Promise<Currency[]> {
+        return await this.currencyRepo.find();
+    }
+    public async getCurrencyById(id: number): Promise<Currency> {
+        return await this.currencyRepo.findOne({
+            where: { id: id },
+        });
+    }
+    public async addCurrency(createCurrnecyDto: CurrencyDto): Promise<Currency> {
+        try {
+            const createdCurrency = this.currencyRepo.create(createCurrnecyDto);
+            const savedCurrency = await this.currencyRepo.save(createdCurrency);
+            return savedCurrency;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    public async UpdateCurrency(id: number, updateCurrencyDto: UpdateCurrencyDto):Promise<UpdateResult> {
+        try {
+            const updatedCurrency = await this.currencyRepo.update(
+                { id: id },
+                { ...updateCurrencyDto },
+            );
+            return updatedCurrency;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    public async DeleteCurrency(id: number) {
+        return await this.currencyRepo.delete({ id: id });
+    }
+}
