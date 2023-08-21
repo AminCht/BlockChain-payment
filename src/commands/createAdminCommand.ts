89b6764b8cdf0819caa7f988396d856e136bd7c7
AminCht <amincht81@gmail.com>
2023-08-21 16:34:12 +0330

    cleaning create admin command code

--- src/commands/createAdminCommand.ts
+++ src/commands/createAdminCommand.ts
@@ -2,15 +2,14 @@ import { Command, CommandRunner, Option } from 'nest-commander';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Role, User } from '../database/entities/User.entity';
-import { ForbiddenException } from '@nestjs/common';
-import { AuthService } from '../auth/auth.service';
+import { AdminService } from '../admin/admin.service';
 
 @Command({ name: 'create-admin' })
 export class CreateAdminCommand extends CommandRunner {
   constructor(
     @InjectRepository(User)
     private readonly userRepo: Repository<User>,
-    private authService: AuthService
+    private adminService: AdminService
   ) {
         super();
     }
@@ -18,21 +17,27 @@ export class CreateAdminCommand extends CommandRunner {
         passedParams: string[],
         options?: Record<string, any>,
     ): Promise<void> {
-        const hashedPassword = await this.authService.hashPassword(passedParams[1]);
+        /*const hashedPassword = await this.authService.hashPassword(passedParams[1]);
         try{
             const user = this.userRepo.create({
                 username: passedParams[0],
                 password: hashedPassword,
                 role: Role.ADMIN,
             });
+            
             await this.userRepo.save(user);
         } catch(error){
             if (error.code === '23505') {
                 throw new ForbiddenException('This UserName has already taken');
             }
             throw error;
-        }
-        
+        }*/
+        const user = this.userRepo.create({
+            username: passedParams[0],
+            password: passedParams[1],
+        });
+        await this.adminService.createAdmin(user);
+
     }
     
 }
