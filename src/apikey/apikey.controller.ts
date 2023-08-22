5f05bdc3de10ed28c38539596becaff0f5eb684b
AminCht <amincht81@gmail.com>
2023-08-22 23:57:02 +0330

    add routes

--- src/apikey/apikey.controller.ts
+++ src/apikey/apikey.controller.ts
@@ -1,9 +1,11 @@
-import { Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Put, Req, UseGuards } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { ApikeyService } from './apikey.service';
 import { ApiKeyAuthGuard } from './guard/apikey.guard';
 import { AuthGuard } from '@nestjs/passport';
 import { EitherGuard } from './guard/either.guard';
+import { Request } from 'express';
+import { ApiKeyRequestDto, ApiKeyUpdateDto } from './dto/apikey.dto';
 
 @ApiTags('ApiKey')
 @Controller('apikey')
@@ -13,7 +15,13 @@ export class ApikeyController {
 
     @Get()
     @UseGuards(EitherGuard)
-    async createApiKey(){
-        console.log('sucess');
+    async createApiKey(@Req() req: Request, dto: ApiKeyRequestDto){
+        return await this.apikeyService.createApiKey(req['user'].user, dto);
+    }
+
+    @Put(':id')
+    @UseGuards(EitherGuard)
+    async updateApiKey(@Req() req: Request, dto: ApiKeyUpdateDto){
+        return await this.apikeyService.updateApiKey(req['user'].user, dto);
     }
 }
