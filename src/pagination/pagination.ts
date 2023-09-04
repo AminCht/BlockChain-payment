6b929ca34d7575108bad39452454e795160c0749
foadas <foad1381@gmail.com>
2023-09-05 02:39:11 +0430

    pagination fixed

--- src/pagination/pagination.ts
+++ src/pagination/pagination.ts
@@ -1,13 +1,10 @@
 import { Repository, SelectQueryBuilder} from "typeorm";
-import { ICondition, PaginationDto } from "./pagination.dto";
+import { ICondition, PaginationDto } from './pagination.dto';
 
 
 export class Pagination {
-    // TODO: whole response shold be created here
     static async paginate<T,G extends ICondition>(repository: Repository<T>, paginationDto: PaginationDto<G>) {
         const { page, pageSize, orderBy, sortOrder, condition } = paginationDto;
-        console.log(page);
-        console.log(pageSize)
         const skip = (page - 1) * pageSize;
         const take = pageSize;
         let query: SelectQueryBuilder<T> = repository.createQueryBuilder();
@@ -17,7 +14,6 @@ export class Pagination {
         query = query.where(condition);
         const pageCount = await query.getCount();
         const data = await query.skip(skip).take(take).getMany();
-        // TODO: Whole response
-        return { data: data, pageCount: pageCount };
+        return { data: data, page: page, pageSize: pageSize, pageCount: pageCount };
     }
 }
