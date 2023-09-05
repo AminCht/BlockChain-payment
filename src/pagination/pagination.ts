0a2c485110278f1ae2db2bf72eb1549b557cc648
AminCht <amincht81@gmail.com>
2023-09-06 01:19:25 +0330

    fix a shity bug

--- src/pagination/pagination.ts
+++ src/pagination/pagination.ts
@@ -1,9 +1,8 @@
-import { Repository, SelectQueryBuilder, getConnection } from "typeorm";
+import { Repository, SelectQueryBuilder } from "typeorm";
 import { ICondition, PaginationDto } from './pagination.dto';
 
 
 export class Pagination {
-    static gooz: any;
     static async paginate<T,G extends ICondition>(repository: Repository<T>, paginationDto: PaginationDto<G>, ) {
         const { page, pageSize, orderBy, sortOrder, condition } = paginationDto;
         const skip = (page - 1) * pageSize;
@@ -12,7 +11,14 @@ export class Pagination {
         if (orderBy) {
             query = query.orderBy(orderBy, sortOrder);
         }
-        console.log(condition)
+        Pagination.rangeConditions(repository, condition, query)     
+        //query = query.where(condition);
+        const pageCount = await query.getCount();
+        const data = await query.skip(skip).take(take).getMany();
+        return { data: data, page: page, pageSize: pageSize, pageCount: pageCount };
+    }
+
+    static rangeConditions(repository: any, condition: object, query: any){
         for (const key in condition) {
             if (condition.hasOwnProperty(key)) {
                 const value = condition[key];
@@ -20,36 +26,31 @@ export class Pagination {
                 if (typeof value === 'object' && 'Gt' || 'Lt' in value) {
                     if('Gt' in value){
                         if(columnTypeFunction === Date){
-                            query = query.where(`${key} >= :value`, { value: value['Gt']});
+                            query = query.andWhere(`${key} >= :${key}1`, { [key+'1']: value['Gt']});
                         }else{
-                            console.log(value['Gt'])
-                            query = query.where(`${key}::numeric >= :value`, { value: value['Gt']});
+                            query = query.andWhere(`${key}::numeric >= :${key}1`, { [key+'1']: value['Gt']});
+                            
                         }
                     }
+                    console.log(query.getParameters())
                     if('Lt' in value){
                         if(columnTypeFunction === Date){
                             
-                            query = query.andWhere(`${key} <= :value`, { value: value['Lt']});
+                            query = query.andWhere(`${key} <= :${key}2`, { [key+'2']: value['Lt']});
                         }
                         else{
-                            console.log(value['Lt'])
-                            query = query.andWhere(`${key}::numeric <= :value`, { value: value['Lt'] });
+                            query = query.andWhere(`${key}::numeric <= :${key}2`, { [key+'2']: value['Lt'] });
                         }
                     }
+                    console.log(query.getParameters())
                 } else {
                     const customCondition: { [key: string]: any } = {};
                     customCondition[key] = value;
                     query = query.andWhere(customCondition)
                 }
             }
-        }
-
-        //query = query.where(condition);
-        const pageCount = await query.getCount();
-        const data = await query.skip(skip).take(take).getMany();
-        return { data: data, page: page, pageSize: pageSize, pageCount: pageCount };
+        } 
     }
-
 }
 
 
