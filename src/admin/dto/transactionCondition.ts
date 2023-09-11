7f474f0fdffb4a1b681751b1a0ff2debe2f25568
AminCht <amincht81@gmail.com>
2023-09-11 20:42:11 +0330

    remove amount filtering

--- src/admin/dto/transactionCondition.ts
+++ src/admin/dto/transactionCondition.ts
@@ -7,34 +7,6 @@ export class TransactionCondition implements ICondition {
     queryToCondition(query: any): ObjectLiteral {
         const result : ObjectLiteral = new Object();
         
-        if (query.amount) {
-            result.amount= query.amount;
-        }
-        if (query.amountGt || query.amountLt) {
-            if(query.amountGt && query.amountLt){
-                result.amount = Raw(
-                    alias => `CAST(${alias} AS DECIMAL) BETWEEN :minAmount AND :maxAmount`,
-                    {
-                        minAmount: query.amountGt,
-                        maxAmount: query.amountLt,
-                    });
-            }
-            else if (query.amountLt) {
-                result.amount = Raw(
-                    alias => `CAST(${alias} AS DECIMAL)  >= :maxAmount`,
-                    {
-                        maxAmount: query.amountLt,
-                    });
-            }
-            else if(query.amountGt){
-                result.amount = Raw(
-                    alias => `CAST(${alias} AS DECIMAL)  >= :minAmount`,
-                    {
-                        minAmount: query.amountGt,
-                    });
-            }
-            
-        }
         if (query.createdAtGt || query.createdAtLt) {
             if(query.createdAtGt && query.createdAtLt){
                 const startDate = new Date(query.createdAtGt);
