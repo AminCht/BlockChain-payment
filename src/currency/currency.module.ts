e73e3aec1ed2c20c78914fb2cb697448c8f4800e
AminCht <amincht81@gmail.com>
2023-09-23 21:06:36 +0330

    add guards to provider

--- src/currency/currency.module.ts
+++ src/currency/currency.module.ts
@@ -4,10 +4,13 @@ import { CurrencyService } from './currency.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Currency } from '../database/entities/Currency.entity';
 import { HttpModule } from '@nestjs/axios';
+import { ApiKeyAuthGuard } from '../apikey/guard/apikey.guard';
+import { EitherGuard } from '../apikey/guard/either.guard';
+import { JwtAuthGuard } from '../auth/guards/jwt.guard';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Currency]), HttpModule],
   controllers: [CurrencyController],
-  providers: [CurrencyService]
+  providers: [CurrencyService,EitherGuard, ApiKeyAuthGuard, JwtAuthGuard]
 })
 export class CurrencyModule {}
