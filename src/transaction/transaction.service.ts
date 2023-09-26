8b2bac2686bc08f67c468bfff01f7f5015e191b3
AminCht <amincht81@gmail.com>
2023-09-26 03:49:19 +0330

    change response format

--- src/transaction/transaction.service.ts
+++ src/transaction/transaction.service.ts
@@ -4,6 +4,7 @@ import { Status, Transaction } from '../database/entities/Transaction.entity';
 import { Repository } from 'typeorm';
 import { TransactionNotFoundException } from './exceptions/transactionNotFound';
 import { User } from '../database/entities/User.entity';
+import { GetTransactionByIdResponseDto } from './dto/transaction.dto';
 enum Statuss {
     Pending = 'Pending',
     Successful = 'Successful',
@@ -23,13 +24,13 @@ export class TransactionService {
                 throw new TransactionNotFoundException(transactionId);
             }
             if(transaction.status == Status.FAILED || transaction.status == Status.SUCCESSFUL){
-                delete transaction.user;
-                delete transaction.wallet_balance_after;
-                delete transaction.wallet_balance_before;
-                const transactionStatus = await this.convertStatusNumber(transaction.status);
-                transaction.amount = String(this.convertAmount(transaction.amount, transaction.currency.decimals));
-                const retunedObject = this.convertTransactionToCustomObject(transaction, transactionStatus);
-                return retunedObject;
+                const transactionStatus = GetTransactionByIdResponseDto.convertStatusNumber(transaction.status);
+                const transactionResponseDto= new GetTransactionByIdResponseDto();
+                transactionResponseDto.amount = GetTransactionByIdResponseDto.convertAmount(transaction.amount, transaction.currency.decimals);
+                transactionResponseDto.status = transactionStatus;
+                transactionResponseDto.created_date = transaction.created_date;
+                transactionResponseDto.expireTime = transaction.expireTime;
+                return transactionResponseDto;
             }
             await this.sleep(4);
         }
@@ -42,21 +43,6 @@ export class TransactionService {
             }, duration*1000);
         });
     }
-    public convertAmount(amount: string, decimal: number){
-        const convertedAmount =BigInt(amount) / BigInt(10) ** BigInt(decimal);
-        return Number(convertedAmount);
-    }
-    public async convertStatusNumber(status: number){
-        if(status == 0){
-            return 'Pending'
-        }
-        else if(status == 1){
-            return 'Sucssesful'
-        }
-        return 'Failed'
-    }
-    public async convertTransactionToCustomObject(transaction: Transaction, status: string){
-        const convertedObject = {id: transaction.id, amount: transaction.amount, status: status, created_date: transaction.created_date, expireTime: transaction.expireTime}
-        return convertedObject;
-    }
+    
+    
 }
