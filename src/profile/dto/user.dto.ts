4c86b41eceb637b5e98f7dfdd15837ffd716257e
AminCht <amincht81@gmail.com>
2023-11-18 19:18:56 +0330

    profiles swagger

--- src/profile/dto/user.dto.ts
+++ src/profile/dto/user.dto.ts
@@ -1,8 +1,32 @@
 import { ApiProperty } from "@nestjs/swagger";
 import { IsNotEmpty } from "class-validator";
+import { Role } from "../../database/entities/User.entity";
 
 export class ChangePasswordDto{
     @ApiProperty()
     @IsNotEmpty()
     newPassword: string
+}
+
+export class GetProfileResponseDto{
+
+    @ApiProperty()
+    id: number
+
+    @ApiProperty()
+    userName: string
+
+    @ApiProperty()
+    role: Role
+
+    @ApiProperty()
+    createdAt: Date
+
+    @ApiProperty()
+    updatedAt: Date
+}
+
+export class ChangePasswordResponseDto{
+    @ApiProperty({default: 'Password Has been Changed'})
+    message: string
 }
