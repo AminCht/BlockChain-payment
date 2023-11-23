c9a24e704a4f79ac2de7b75f29db6aa2b598cf92
foadas <foad1381@gmail.com>
2023-11-23 10:21:51 -0500

    added swagger ro balance and withdraw

--- src/withdraw/dto/withdraw.dto.ts
+++ src/withdraw/dto/withdraw.dto.ts
@@ -32,3 +32,32 @@ export class UpdateWithdrawRequestDto{
     dst_wallet?: string
 
 }
+
+export class GetWithdrawDto{
+
+    @ApiProperty()
+    id: number;
+    @ApiProperty()
+    status: boolean;
+    @ApiProperty()
+    tx_hash: string;
+    @ApiProperty()
+    tx_url: string;
+    @ApiProperty()
+    created_at: Date;
+    @ApiProperty()
+    updated_at: Date;
+
+    @ApiProperty()
+    @IsNumberString()
+    @IsNotEmpty()
+    amount: string
+
+    @ApiProperty()
+    @IsNotEmpty()
+    currencyId: number;
+    @ApiProperty()
+    @IsNotEmpty()
+    dst_wallet: string
+
+}
--- src/withdraw/withdraw.controller.ts
+++ src/withdraw/withdraw.controller.ts
@@ -1,13 +1,18 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Put, Req, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { WithdrawService } from './withdraw.service';
-import { CreateWithdrawDto, UpdateWithdrawRequestDto } from './dto/withdraw.dto';
+import { CreateWithdrawDto, GetWithdrawDto, UpdateWithdrawRequestDto } from "./dto/withdraw.dto";
 import { Request } from 'express';
+import { ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
+import { CreateTicketResponseDto } from "../ticket/dto/ticket.dto";
 
+@ApiTags('withdraw')
 @Controller('withdraw')
 export class WithdrawController {
     constructor ( private withdrawService: WithdrawService){}
 
+    @ApiOperation({ summary: 'get all withdraws' })
+    @ApiResponse({ status: 200, type: GetWithdrawDto })
     @UseGuards(AuthGuard(['jwt']))
     @Get()
     async getAllWithdraws(@Req() req: Request){
@@ -16,17 +21,22 @@ export class WithdrawController {
 
     @UseGuards(AuthGuard(['jwt']))
     @Post()
+    @ApiOperation({ summary: 'post a withdraw' })
     async createWithdraw(@Body() dto: CreateWithdrawDto, @Req() req: Request){
         return await this.withdrawService.createWithdraw(dto, req['user']);
     }
 
     @UseGuards(AuthGuard(['jwt']))
+    @ApiOperation({ summary: 'patch a withdraw' })
+    @ApiResponse({ status: 200, type: UpdateWithdrawRequestDto })
     @Patch(':id')
     async cancelWithdraw(@Param('id') withdrawId: string){
         return await this.withdrawService.cancelWithdraw(Number(withdrawId));
     }
 
     @UseGuards(AuthGuard(['jwt']))
+    @ApiOperation({ summary: 'put a withdraw' })
+    @ApiResponse({ status: 200, type: UpdateWithdrawRequestDto })
     @Put(':id')
     async updateWithdraw(@Body() dto: UpdateWithdrawRequestDto, @Param('id') id: string, @Req() req: Request){
         return await this.withdrawService.updateWithdraw(dto, Number(id), req['user']);
